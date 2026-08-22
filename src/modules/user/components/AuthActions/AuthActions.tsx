aa911fb0d7a9f5b680baa5643100cebbddeb7d16
hyagoassiz <hyagoassiz@hotmail.com>
2026-08-22 18:14:02 -0300

    adicionado AuthActions

---
+++ src/modules/user/components/AuthActions/AuthActions.tsx
@@ -0,0 +1,65 @@
+import { LoadingButton } from "@mui/lab";
+import { Box, Divider, Link, Typography } from "@mui/material";
+
+interface AuthActionsProps {
+  linkHref: string;
+  isLoading: boolean;
+  buttonLabel: string;
+  linkLabel: string;
+  linkDescription: string;
+  onClick(): void;
+}
+
+export function AuthActions({
+  linkHref,
+  isLoading,
+  buttonLabel,
+  linkLabel,
+  linkDescription,
+  onClick,
+}: AuthActionsProps) {
+  return (
+    <Box>
+      <LoadingButton
+        type="submit"
+        variant="contained"
+        fullWidth
+        sx={{ mt: 1, py: 1.5, textTransform: "none", fontWeight: 700 }}
+        size="large"
+        disabled={isLoading}
+        loading={isLoading}
+        loadingPosition="center"
+        onClick={onClick}
+      >
+        {buttonLabel}
+      </LoadingButton>
+
+      <Divider
+        sx={{
+          width: "100%",
+          height: "1px",
+          marginTop: "20px",
+        }}
+      />
+
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        sx={{
+          mt: 2,
+          textAlign: "center",
+        }}
+      >
+        {linkDescription}{" "}
+        <Link
+          href={linkHref}
+          sx={{
+            color: "inherit",
+          }}
+        >
+          {linkLabel}
+        </Link>
+      </Typography>
+    </Box>
+  );
+}
